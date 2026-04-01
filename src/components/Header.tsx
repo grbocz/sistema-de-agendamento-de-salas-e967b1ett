2773c79122fc9d2ba88a198250194742a1fdfeca
Skip <skip@usecurling.com>
2026-04-01 13:23:26 +0000

    Auto-commit after full sync (build simulation)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,7 @@ export function Header() {
             <CalendarDays className="h-5 w-5" />
           </div>
           <span className="hidden font-bold sm:inline-block text-lg tracking-tight">
-            Ethimos Campinas1
+            Ethimos Campinas
           </span>
         </div>
 
